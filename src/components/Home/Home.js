44290e75ba60cec5476f5f949f2b914c1b67815d
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-04-26 00:56:27 +0600

    finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,68 +3,103 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
-
-
 const Home = () => {
-    const [selections, setSelections] = useState([]);
-    const { register, handleSubmit } = useForm();
- 
-            
-    useEffect(() =>
-    {
-        axios.get("https://job-task-server-pearl.vercel.app/sectors")
-        .then(res => setSelections(res.data))  
-    } ,[])
+  const [selections, setSelections] = useState([]);
+  const { control, register, handleSubmit } = useForm({
+    defaultValues: {
+      name: "",
+      sectors: [],
+      checked: false,
+    },
+  });
 
+  console.log(control);
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("formData"));
+    if (storedData) {
+      // const { name, sectors, checked } = storedData;
+      // control.setValue("name", name);
+      // control.setValue("sectors", sectors);
+      // control.setValue("checked", checked);
+    }
+  });
+
+ 
+  useEffect(() => {
+    axios
+      .get("https://job-task-server-pearl.vercel.app/sectors")
+      .then((res) => setSelections(res.data));
+  }, []);
 
   const onSubmit = (data) => {
-   toast.success("Successfully Fill-up")
-    console.log(data);
+    const newData = {
+      ...data,
+    };
+
+    console.log(newData);
+
+    axios.post("https://job-task-server-pearl.vercel.app/selections", newData).then((res) => {
+      if (res.data.success) {
+        localStorage.setItem("formData", JSON.stringify(data));
+        toast.success("Successfully Fill-up");
+      } else {
+        toast.error("Please fill out all required fields");
+      }
+    });
   };
 
- 
   return (
-    <div className="my-20">
-      <div className="grid justify-center align-center">
-        <form onSubmit={handleSubmit(onSubmit)} className="">
+    <div className="my-20 grid justify-center align-center text-black">
+      <div className="card w-96 bg-base-100 shadow-2xl p-8 shadow-slate-600">
+        <h2 className="font-bold my-4">
+          Please enter your name and pick the Sectors you are currently involved
+          in.
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Name:</span>
+              <span className="label-text font-bold">Name:</span>
             </label>
             <input
               {...register("name", { required: true })}
               type="text"
               id="name"
               placeholder="Name"
-              className="input border-info mb-3 input-bordered max-w-xs rounded-none"
+              className="input border-black mb-3 input-bordered  max-w-xs rounded-none"
+              required
             />
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text">Sectors:</span>
+              <span className="label-text font-bold">Sectors:</span>
             </label>
-             <select multiple id="sector" onChange={onchange}
+            <select
+              multiple
+              id="sectors"
               {...register("sectors", { required: true })}
-              className="select select-info w-full max-w-xs rounded-none"
+              className="select  w-full max-w-xs rounded-none  border-black text-black"
+              required
             >
-               {[...selections].map(selection => (
-            <option key={selection._id} value={selection.value} options={selection.label}>
-              
-            </option>
-          ))}
-            </select> 
-           
-
+              {[...selections].map((selection) => (
+                <option
+                  key={selection._id}
+                  value={selection.value}
+                  label={selection.label}
+                ></option>
+              ))}
+            </select>
           </div>
           <div className="flex mt-3 ">
-            <input id="termsAgreed"
+            <input
+              id="termsAgreed"
               {...register("checked", { required: true })}
               type="checkbox"
-              className="radio radio-primary"
+              className="radio
+               radio-black border-black"
+              required
             />
             <p className="ml-2">Accept terms & Conditions</p>
           </div>
-
           <div className="form-control mt-6">
             <button
               type="submit"
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-black text-white uppercase">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl">Tasks</a>
+    <a className="btn btn-ghost normal-case text-xl">Sectors</a>
   </div>
   <div className="flex-none">
     <ul className="menu menu-horizontal px-1">
