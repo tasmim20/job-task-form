3d1e55c926d37cb935cf00f67f47ddfb512d813e
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-04-26 10:29:37 +0600

    views added

--- src/components/Home/Views.js
+++ src/components/Home/Views.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { AiFillEdit } from "react-icons/ai";
 
 const Views = () => {
-    return (
-        <div>
-            <h2>views</h2>
-        </div>
-    );
+  const [sectors, setSectors] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://job-task-server-pearl.vercel.app/selections")
+      .then((res) => setSectors(res.data.result));
+  }, []);
+
+  return (
+    <div className=" max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12 test-red">
+      <div className="overflow-x-auto ">
+        <table className="table  w-full">
+          <thead className=" bg-gray-700">
+            <tr className="text-2xl">
+              <th></th>
+              <th>Name</th>
+              <th>Sectors</th>
+              <th>Terms Agreed</th>
+              <th>Edit </th>
+            </tr>
+          </thead>
+          <tbody>
+            {[...sectors].map((row) => (
+              <tr className="bg-red" key={row._id}>
+                <th></th>
+                <td>{row.name}</td>
+                <td>{row.sectors.join(",")}</td>
+                <td>{row.checked ? "Yes" : "No"}</td>
+                {/* <td><button className="btn bg-lime btn-outline btn-black border-black hover:bg-black hover:text-white rounded-none ">Edit</button></td>  */}
+                <td>
+                  <AiFillEdit className="text-black "></AiFillEdit>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Views;
